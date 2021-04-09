e78ae4a95785a55ade9242cf0a0118581b0b79ba
Bakyt <djumabaevb@gmail.com>
2021-04-09 12:03:51 +0600

    gradle sync

--- app/src/main/java/com/example/locationtest/MainActivity.java
+++ app/src/main/java/com/example/locationtest/MainActivity.java
@@ -3,22 +3,46 @@ package com.example.locationtest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveResourceClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GeoDataClient mGeoDataClient;
+    private PlaceDetectionClient mPlaceDetectionClient;
+    private static final int RC_DRIVE_PERMS = 7 ;
+    private GoogleSignInClient mSignInClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        GoogleSignInOptions options =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestScopes(Drive.SCOPE_FILE)
+                        .build();
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -72,10 +104,37 @@ public class MainActivity extends AppCompatActivity {
                         public void onCancel(DialogInterface dialog) {
                             Toast.makeText(MainActivity.this, "No services",
                                     Toast.LENGTH_LONG).show();
+                            finish();
                         }
                     });
-            assert errorDialog != null;
             errorDialog.show();
+        } else {
+            Toast.makeText(MainActivity.this, "All is good",
+                    Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void createDriveFile() {
+        // Get currently signed in account (or null)
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        // Synchronously check for necessary permissions
+        if (!GoogleSignIn.hasPermissions(account, Drive.SCOPE_FILE)) {
+            // Note: this launches a sign-in flow, however the code to detect
+            // the result of the sign-in flow and retry the API call is not
+            // shown here.
+            GoogleSignIn.requestPermissions(this, RC_DRIVE_PERMS,
+                    account, Drive.SCOPE_FILE);
+            return;
+        }
+
+        DriveResourceClient client = Drive.getDriveResourceClient(this, account);
+        client.createContents()
+                .addOnCompleteListener(new OnCompleteListener<DriveContents>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DriveContents> task) {
+                        // ...
+                    }
+                });
     }
 }
